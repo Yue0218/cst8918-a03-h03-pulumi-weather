155b33006843063c39400a1a8617bc97778707af
buru0003 <buru0003@algonquinlive.com>
2025-01-29 19:37:29 -0500

    build: Update the IaC Definition

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,8 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
-import * as docker from '@pulumi/docker'
+import * as docker from '@pulumi/docker';
 import * as containerinstance from '@pulumi/azure-native/containerinstance'
+import * as cache from '@pulumi/azure-native/cache';
+
 
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
@@ -10,6 +12,7 @@ const appPath = config.require('appPath')
 const prefixName = config.require('prefixName')
 const imageName = prefixName
 const imageTag = config.require('imageTag')
+
 // Azure container instances (ACI) service does not yet support port mapping
 // so, the containerPort and publicPort must be the same
 const containerPort = config.requireNumber('containerPort')
@@ -19,6 +22,30 @@ const memory = config.requireNumber('memory')
 
 // Create a resource group.
 const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
+// Create a managed Redis service
+const redis = new cache.Redis(`${prefixName}-redis`, {
+  name: `${prefixName}-weather-cache`,
+  location: 'canadacentral',
+  resourceGroupName: resourceGroup.name,
+  enableNonSslPort: true,
+  redisVersion: 'Latest',
+  minimumTlsVersion: '1.2',
+  redisConfiguration: {
+    maxmemoryPolicy: 'allkeys-lru',
+  },
+  sku: {
+    name: 'Basic',
+    family: 'C',
+    capacity: 0,
+  },
+})
+// Extract the auth creds from the deployed Redis service
+const redisAccessKey = cache
+  .listRedisKeysOutput({ name: redis.name, resourceGroupName: resourceGroup.name })
+  .apply(keys => keys.primaryKey)
+// Construct the Redis connection string to be passed as an environment variable in the app container
+const redisConnectionString = pulumi.interpolate`rediss://:${redisAccessKey}@${redis.hostName}:${redis.sslPort}`
+
 
 // Create the container registry.
 const registry = new containerregistry.Registry(`${prefixName}ACR`, {
@@ -41,21 +68,20 @@ const registryCredentials = containerregistry
       password: creds.passwords![0].value!,
     }
   })
-
   // Define the container image for the service.
-const image = new docker.Image(`${prefixName}-image`, {
-  imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
-  build: {
-    context: appPath,
-    platform: 'linux/amd64',
-  },
-  registry: {
-    server: registry.loginServer,
-    username: registryCredentials.username,
-    password: registryCredentials.password,
-  },
-})
-
+  const image = new docker.Image(`${prefixName}-image`, {
+    imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
+    build: {
+      context: appPath,
+      platform: 'linux/amd64',
+    },
+    registry: {
+      server: registry.loginServer,
+      username: registryCredentials.username,
+      password: registryCredentials.password,
+    },
+  });
+  
 // Create a container group in the Azure Container App service and make it publicly accessible.
 const containerGroup = new containerinstance.ContainerGroup(
   `${prefixName}-container-group`,
@@ -85,9 +111,16 @@ const containerGroup = new containerinstance.ContainerGroup(
             name: 'PORT',
             value: containerPort.toString(),
           },
+          // existing vars ...
+        {
+           name: 'REDIS_URL',
+             value: redisConnectionString
+           },
           {
-            name: 'WEATHER_API_KEY',
-            value: config.requireSecret('weatherApiKey')
+            
+              name: 'WEATHER_API_KEY',
+              value: config.requireSecret('weatherApiKey')
+            
           },
         ],
         resources: {
@@ -111,6 +144,7 @@ const containerGroup = new containerinstance.ContainerGroup(
   },
 )
 
+// Export the service's IP address, hostname, and fully-qualified URL.
 export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
 export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
 export const url = containerGroup.ipAddress.apply(
